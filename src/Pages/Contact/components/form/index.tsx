f917be676220992350f5009bc83f06e8d29a536a
Sajjad-iq <rasooool59@gmail.com>
2025-06-01 12:17:47 +0300

    Remove unused React import from Contact form component for cleaner code.

--- src/Pages/Contact/components/form/index.tsx
+++ src/Pages/Contact/components/form/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { ProjectsSecondaryWrapper } from '../../../../Components/common/ProjectsSecondaryWrapper.styled'
 import { Column } from '../../../../Components/common/Column.styled'
 import { H5 } from '../../../../Components/common/H5.styled'
